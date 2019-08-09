4ac5bc737c83984bf11b867b72ecb74dd1edb91d
Steve Boardwell <steve.boardwell@datameer.com>
2019-08-09 14:59:01 +0200

    Checks should only look at Major.Minor.Patch

--- scripts/version_util.sh
+++ scripts/version_util.sh
@@ -25,7 +25,9 @@ function version_gt() {
   if [[ "$eq" == "eq" ]] && [[ "$v1" == "$v2" ]]; then
     return
   else
-    test "$(printf '%s\n%s\n' "$v1" "$v2" | sort -V | head -n 1)" != "$v1"
+    # we are only interested in the Major Minor Patch values
+    # strip away any '-alpha' or '-beta' with ${v1//-*} and ${v2//-*}
+    test "$(printf '%s\n%s\n' "${v1//-*}" "${v2//-*}" | sort -V | head -n 1)" != "${v1//-*}"
   fi
 }
 
@@ -318,9 +320,9 @@ function ensure_source_version_gt_target_version() {
   local target=$2
   local sourceVersion targetVersion
   checkout_branch $source -q
-  sourceVersion=$(run_cmd /showvariable FullSemVer)
+  sourceVersion=$(run_cmd /showvariable SemVer)
   checkout_branch $target -q
-  targetVersion=$(run_cmd /showvariable FullSemVer)
+  targetVersion=$(run_cmd /showvariable SemVer)
   version_gt $sourceVersion $targetVersion || die "Source branch version is lower than target branch version:
   $sourceVersion <- source ($source)
   vs
